889f7bdf578cb34c804dc65f260792579d6e3411
AbhayGoyal <goyalabhay97@gmail.com>
2017-10-04 18:54:08 +0530

    Add files via upload

---
+++ src/student/teacher/portal/StudentTeacherPortal.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package student.teacher.portal;
+import Common.*;
+//import stud.*;
+//Teacher;
+
+import Common.Log;
+import Common.logged;
+
+/**
+ *
+ * @author Abhay Goyal
+ */
+public class StudentTeacherPortal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Log l=new Log();
+        l.setVisible(true);
+        /*logged l1=new logged();
+        l1.setVisible(true);*/
+    }
+    
+}
